f69c77a5fddf09a2f22fc21ea57b37f0a4cedb01
Tero Laxström <tlax@neonpeons.com>
2017-05-25 11:50:58 +0300

    Chapter 8 exercises done
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- chap08/wordnumber.hs
+++ chap08/wordnumber.hs
@@ -3,11 +3,24 @@ module WordNumber where
 import Data.List (intersperse)
 
 digitToWord :: Int -> String
-digitToWord n = undefined
+digitToWord n = case n of
+  0 -> "zero"
+  1 -> "one"
+  2 -> "two"
+  3 -> "three"
+  4 -> "four"
+  5 -> "five"
+  6 -> "six"
+  7 -> "seven"
+  8 -> "eight"
+  9 -> "nine"
 
 digits :: Int -> [Int]
-digits n = undefined
+digits n = go n []
+  where go n acc
+          | n == 0 = acc
+          | otherwise = go (n `div` 10) ((n `mod` 10) : acc)
 
 wordNumber :: Int -> String
-wordNumber n = undefined
+wordNumber n = concat $ intersperse "-" $ map digitToWord $ digits n
 
