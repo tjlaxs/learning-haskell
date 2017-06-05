a752d728d77b6a5d93876f050a847e06650ff85c
Tero Laxström <tlax@neonpeons.com>
2017-06-05 19:30:31 +0300

    Zipping exercises in chapter 9
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- chap09/zipping.hs
+++ chap09/zipping.hs
@@ -7,9 +7,12 @@ myZip x y = go x y []
 
 -- 2. Custom version of zipWith.
 myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
-myZipWith = undefined
+myZipWith f x y = go x y []
+  where go xs [] acc = reverse acc
+        go [] xs acc = reverse acc
+        go (x:xs) (y:ys) acc = go xs ys (f x y : acc)
 
 -- 3. Rewrite 1st with 2nd
 myZip' :: [a] -> [b] -> [(a, b)]
-myZip' = undefined
+myZip' = myZipWith (\x y -> (x, y))
 
