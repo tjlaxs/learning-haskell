38931c7c969ccf8c0ed99fdc283e374c3ad9803e
Tero Laxström <tlax@neonpeons.com>
2017-06-06 18:57:11 +0300

    Cipher module spec
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

---
+++ chap09/chiper.hs
@@ -0,0 +1,15 @@
+module Cipher where
+
+import Data.Char
+
+caesar :: Int -> String -> String
+caesar  = undefined
+
+unCaesar :: Int -> String -> String
+unCaesar = undefined
+
+-- for testing
+rot13 = caesar 13
+c12 = caesar 12
+uc12 = unCaesar 12
+
