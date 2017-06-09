be08059e83400c5c2b6850a5d4652d38ecc90b43
Tero Laxström <tlax@neonpeons.com>
2017-06-09 21:43:28 +0300

    Setup for chapter warmup exercises
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

---
+++ chap10/chapterwarmup.hs
@@ -0,0 +1,19 @@
+-- Warmup and review
+
+-- 1.
+stops = "pbtdkg"
+vowels = "aeiou"
+
+-- 1a. Write a function to create all possible 3-tuples of  stop-vowel-stop
+--    combinations.
+stopVowelStop = undefined
+
+-- 1b. Modify that function so that it only returns the combinations that begin
+--    with a p.
+stopVowelStop' = undefined 
+
+-- 1c. Write function to generate possible noun-vowel-noun combinations.
+nouns = undefined
+
+nounVowelNoun = undefined
+
