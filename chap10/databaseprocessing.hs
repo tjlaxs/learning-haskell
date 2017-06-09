ad9529e7d0c629320228a858911b73d92545bbc2
Tero Laxström <tlax@neonpeons.com>
2017-06-09 20:33:48 +0300

    Finished with fold exercises
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- chap10/databaseprocessing.hs
+++ chap10/databaseprocessing.hs
@@ -1,3 +1,4 @@
+-- Practise folds
 import Data.Time
 
 data DatabaseItem = DbString String
@@ -9,6 +10,7 @@ theDatabase :: [DatabaseItem]
 theDatabase =
   [ DbDate (UTCTime (fromGregorian 1911 5 1) (secondsToDiffTime 34123))
   , DbNumber 9001
+  , DbNumber 42
   , DbString "Hello, world!"
   , DbDate (UTCTime (fromGregorian 1921 5 1) (secondsToDiffTime 34123))
   ]
@@ -16,22 +18,30 @@ theDatabase =
 -- 1. Write a function that filters for DbDate valuers and returns a list of the
 --    UTCTime values inside them.
 filterDbDate :: [DatabaseItem] -> [UTCTime]
-filterDbDate = undefined
+filterDbDate = foldr f []
+  where f (DbDate d) acc = d : acc
+        f _ acc = acc
 
 -- 2. Write a function that filters for DbNumber values and returns a list of
 --    the Integer values inside them.
 filterDbNumber :: [DatabaseItem] -> [Integer]
-filterDbNumber = undefined
+filterDbNumber = foldr f []
+  where f (DbNumber n) acc = n : acc
+        f _ acc = acc
 
 -- 3. Write a function that gets the most recent date.
 mostRecent :: [DatabaseItem] -> UTCTime
-mostRecent = undefined
+mostRecent d = foldr max (head dates) (tail dates)
+  where dates = filterDbDate d
 
 -- 4. Write a function that sums all of the DbNumber values.
 sumDb :: [DatabaseItem] -> Integer
-sumDb = undefined
+sumDb = foldr (+) 0 . filterDbNumber
 
 -- 5. Write a function that gets the average of the DbNumber values.
 avgDb :: [DatabaseItem] -> Double
-avgDb = undefined
+avgDb = avg . filterDbNumber
+  where avg xs = fromInteger (s xs) / l xs
+        s xs = foldr (+) 0 xs
+        l xs = foldr (\ _ y -> y + 1) 0 xs
 
