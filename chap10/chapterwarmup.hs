087e0cb342e9350d148cb035e2f5b9f6dc71ca9f
Tero Laxström <tlax@neonpeons.com>
2017-06-09 23:00:32 +0300

    Solutions for warmups
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- chap10/chapterwarmup.hs
+++ chap10/chapterwarmup.hs
@@ -6,14 +6,27 @@ vowels = "aeiou"
 
 -- 1a. Write a function to create all possible 3-tuples of  stop-vowel-stop
 --    combinations.
-stopVowelStop = undefined
+stopVowelStop = zip3 s1 v s2
+  where ls = length stops
+        lv = length vowels
+        s1 = concat $ map (take (ls * lv) . repeat) stops
+        v = concat $ take ls . repeat $ concat $ map (take ls . repeat) vowels
+        s2 = concat $ take (ls * lv) $ repeat stops
 
 -- 1b. Modify that function so that it only returns the combinations that begin
 --    with a p.
-stopVowelStop' = undefined 
+stopVowelStop' = filter (\ (p, _, _) -> p == 'p') stopVowelStop 
 
 -- 1c. Write function to generate possible noun-vowel-noun combinations.
-nouns = undefined
+nouns = filter (`notElem` vowels) ['a'..'z']
 
-nounVowelNoun = undefined
+nounVowelNoun = zip3 s1 v s2
+  where stops = nouns
+        ls = length stops
+        lv = length vowels
+        s1 = concat $ map (take (ls * lv) . repeat) stops
+        v = concat $ take ls . repeat $ concat $ map (take ls . repeat) vowels
+        s2 = concat $ take (ls * lv) $ repeat stops
+
+
 
